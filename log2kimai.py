130e5ec5fee0ceac5b6db0fbc362be598572cf42
Rickmer Frier <info@rickmerfrier.de>
2014-12-03 22:59:22 +0100

    added activity and project listing, more validation and positional argument(s)

--- log2kimai.py
+++ log2kimai.py
@@ -32,7 +32,7 @@ class kimaiMessage(object):
         result = opener.open(request)
         self.session = opener
         
-        #extract user id from request body
+        #extract user id from result body
         for line in result:
             if search(r"userID", line):
                 self.userid = sub(r'^.*([0-9]{9}).*\n$', r'\1', line)
@@ -42,6 +42,20 @@ class kimaiMessage(object):
         except:
             exit("userID was not found")
 
+        #extract project and activities dicts from result body
+        self.projects = {}
+        self.activity = {}
+        for line in result:
+            if search(r'buzzer_preselect_project\(', line):
+                match = sub(r".*buzzer_preselect_project\(([0-9]{1,}),'(.*)',.*\n", r'\1,\2', line)
+                (idnum, name) = tuple(match.split(','))
+                if not self.projects.has_key(int(idnum)):
+                    self.projects[int(idnum)] = name
+            elif search(r'buzzer_preselect_activity\(', line):
+                match = sub(r".*buzzer_preselect_activity\(([0-9]{1,}),'(.*)'\).*\n", r'\1,\2', line)
+                (idnum, name) = tuple(match.split(','))
+                if not self.activity.has_key(int(idnum)):
+                    self.activity[int(idnum)] = name
 
     def __del__(self):
         """
@@ -89,9 +103,13 @@ class kimaiMessage(object):
         self.session.open(request)
 
 def main():
-        
+    
+    # parse commandline arguments
     cmd_parser = ArgumentParser(description='Log Work to a Kimai instance')
     cmd_parser.add_argument('--configFile', type=str, default='log2kimai.cfg')
+    cmd_parser.add_argument('-v', '--verbose', action='store_true')
+    cmd_parser.add_argument('-d', '--dry', action='store_true', help='dryrun; just validating input')
+    cmd_parser.add_argument('action', nargs='+', help='add, info projects/activities')
     cmd_args = cmd_parser.parse_args()
     config_file = ConfigParser()
 
@@ -100,51 +118,75 @@ def main():
     except IOError:
         exit('Error: Specified config file was not found or not readable.')
 
-    add_list = []
-    
-    # read and validate stdin  
-    line_counter = 0
-    for line in stdin.readlines():
-        line_counter += 1
-        line = sub(r'\n', r'', ''.join(line))
-        input_list = line.split('|')
-        # validate number of arguments per line
-        if len(input_list) != 5:
-            print ' '.join(['error parsing stdin line', str(line_counter)])
+    # info mode
+    if len(cmd_args.action) == 2 and cmd_args.action[0] == 'info':
+        km = kimaiMessage(config_file.get('kimai','baseurl'), 
+                          config_file.get('kimai', 'user'), 
+                          config_file.get('kimai', 'pass'))
+        if cmd_args.action[1] == 'activities':
+            for key in km.activity:
+                print ' '.join([str(key), ':', km.activity[key]])
+        elif cmd_args.action[1] == 'projects':
+            for key in km.projects:
+                print ' '.join([str(key), ':', km.projects[key]])
+        exit()
+    elif len(cmd_args.action) == 1 and cmd_args.action[0] == 'add':       
+        # add mode
         
-        # validate startdate
-        try:
-            datetime_start = datetime.strptime(input_list[0], '%y%m%d-%H%M')
-        except ValueError:
-            exit(' '.join(['Error parsing start date (format is supposed to be YYYYmmdd-HHmm)', str(line_counter)]))
+        # check stdin
+        if stdin.isatty():
+            exit()
 
-        # validate duration 
-        try:
-            datetime_end = datetime_start + timedelta(minutes=+int(input_list[1]))
-        except ValueError:
-            exit(' '.join(['Error parsing duration (supposed to be an integer) in line', str(line_counter)]))
+        # read and validate stdin  
+        add_list = []
+        line_counter = 0
+        for line in stdin.readlines():
+            line_counter += 1
+            line = sub(r'\n', r'', ''.join(line))
+            input_list = line.split('|')
+            # validate number of arguments per line
+            if len(input_list) != 5:
+                print ' '.join(['error parsing stdin line', str(line_counter)])
+            
+            # validate startdate
+            try:
+                datetime_start = datetime.strptime(input_list[0], '%y%m%d-%H%M')
+            except ValueError:
+                exit(' '.join(['Error parsing start date (format is supposed to be YYYYmmdd-HHmm)', str(line_counter)]))
 
-        # validate project id
-        try:
-            project_id = int(input_list[2])
-        except ValueError:
-            exit(' '.join(['Error parsing project_id (supposed to be an integer) in line', str(line_counter)]))
-        
-        # validate activity id
-        try:
-            activity_id = int(input_list[3])
-        except ValueError: 
-            exit(' '.join(['Error parsing activity_id (supposed to be an integer) in line', str(line_counter)]))
-        
-        comment = input_list[4]
-        add_list.append((datetime_start, datetime_end, project_id, activity_id, comment)) 
-        
+            # validate duration 
+            try:
+                datetime_end = datetime_start + timedelta(minutes=+int(input_list[1]))
+            except ValueError:
+                exit(' '.join(['Error parsing duration (supposed to be an integer) in line', str(line_counter)]))
 
-    km = kimaiMessage(config_file.get('kimai','baseurl'), 
-                      config_file.get('kimai', 'user'), 
-                      config_file.get('kimai', 'pass'))
-    for (start, end, pid, aid, comment) in add_list:
-        km.logWork(start, end, pid, aid, comment)
+            # validate project id
+            try:
+                project_id = int(input_list[2])
+            except ValueError:
+                exit(' '.join(['Error parsing project_id (supposed to be an integer) in line', str(line_counter)]))
+            
+            # validate activity id
+            try:
+                activity_id = int(input_list[3])
+            except ValueError: 
+                exit(' '.join(['Error parsing activity_id (supposed to be an integer) in line', str(line_counter)]))
+            
+            comment = input_list[4]
+            add_list.append((datetime_start, datetime_end, project_id, activity_id, comment)) 
+            
+            if cmd_args.verbose:
+                print (datetime_start, datetime_end, project_id, activity_id, comment)
+        
+        if not cmd_args.dry and len(cmd_args.action) > 0 and cmd_args[0] == 'add':
+            # send requests to log work 
+            km = kimaiMessage(config_file.get('kimai','baseurl'), 
+                              config_file.get('kimai', 'user'), 
+                              config_file.get('kimai', 'pass'))
+            for (start, end, pid, aid, comment) in add_list:
+                km.logWork(start, end, pid, aid, comment)
+    else:
+        cmd_parser.print_usage()
     
 if __name__ == "__main__":
     main()
